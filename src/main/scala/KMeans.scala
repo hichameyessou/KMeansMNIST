13391f1a5ea6323405ccf5929afcfed32efdff50
Hichame Yessou <hichame.yessou@gmail.com>
2019-01-04 16:51:02 -0800

    Save clusters

--- src/main/scala/KMeans.scala
+++ src/main/scala/KMeans.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.mllib.clustering.KMeans
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.{SparkConf, SparkContext}
+import java.io._
 
 object KMeansScala {
 
@@ -9,17 +10,23 @@ object KMeansScala {
     val conf:SparkConf = new SparkConf().setAppName("KMeansScala").setMaster("local")
     val sc:SparkContext = new SparkContext(conf)
 
-
     // Load data
     val data = sc.textFile("src/main/resources/mnist_test.csv")
 
     var lines = data
       .map(l => Vectors.dense(l.split(",").map(_.toDouble))).cache()
 
-    var clusters = KMeans.train(lines, 10, 40)
+    var clusters = KMeans.train(lines, 10, 100)
 
-    clusters.save(sc,"target/KMeansModel")
+    printToFile(new File("target/clusters.txt")) { p =>
+      clusters.clusterCenters.foreach(p.println)
+    }
+
     println(clusters.clusterCenters.mkString("\n"))
   }
 
+  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
+    val p = new java.io.PrintWriter(f)
+    try { op(p) } finally { p.close() }
+  }
 }
